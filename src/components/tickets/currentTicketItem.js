b611e335874d9cd56f00f5ad47ec112c2237136e
Shelley Arnold <shelle.kay@gmail.com>
2019-06-07 21:53:53 -0500

    added reassign functionality

--- src/components/tickets/currentTicketItem.js
+++ src/components/tickets/currentTicketItem.js
@@ -27,7 +27,21 @@ export default class CurrentTicketItem extends Component {
       solutionNotes: this.state.solutionNotes,
       id: this.props.item.id
     })
-  }
+  };
+
+  handleReassign = () => {
+    this.props.editTicket({
+      userId: this.props.item.userId,
+      classId: this.props.item.classId,
+      ticketComplete: false,
+      ticketTitle: this.props.item.ticketTitle,
+      ticketBody: this.props.item.ticketBody,
+      submitTime: this.props.item.submitTime,
+      linked: this.props.item.linked,
+      solutionNotes: this.state.solutionNotes,
+      id: this.props.item.id
+    })
+  };
 
   render() {
     return (
@@ -42,10 +56,11 @@ export default class CurrentTicketItem extends Component {
               <TextArea
                 label='Solution Notes'
                 placeholder='Solution Notes'
+                value={ this.props.item.solutionNotes }
                 onChange={ (e) => this.setState({ solutionNotes: e.target.value }) }
               />
             </Form>
-            <Button className="btn-margin">Reassign</Button>
+            <Button className="btn-margin" onClick={ this.handleReassign } >Reassign</Button>
             <Button className="btn-margin" onClick={ this.handleComplete } >Complete</Button>
           </Card.Content>
         </Card>
