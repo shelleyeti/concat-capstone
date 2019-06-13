56badf2a96e52812fce778530828e50996b8bee7
Shelley Arnold <shelle.kay@gmail.com>
2019-06-13 18:44:43 -0500

    resolved

--- src/components/applicationViews.js
+++ src/components/applicationViews.js
@@ -105,6 +105,7 @@ class ApplicationViews extends Component {
       .then((ticket) => {
         this.props.history.push("/tickets/my-tickets")
         this.setState(newState)
+        //return ticket so it can be used in the form
         return ticket;
       });
   };
